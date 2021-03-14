3cc994b3be435e8237aa39b548c11acda27edbd0
bryan <bryan.barbou@hotmail.fr>
2021-03-14 14:07:23 +0100

    ADD zoneModel
    It is now possible to edit/delete zones and get the festival's zones

--- server/models/zoneModel.js
+++ server/models/zoneModel.js
@@ -9,5 +9,23 @@ module.exports = {
         const text = 'INSERT INTO "Zone" ("libelleZone","FK_idFestival") VALUES ($1,$2);'
         const queryValues = [libelleZone, FK_idFestival]
         clientUsed.query(text,queryValues)
-    }
+    },
+    //Update a zone
+    updateZone: async (idZone,libelleZone,client)=>{
+        const clientUsed = await DB.getPoolClient(client)
+        const queryText = `UPDATE "Zone" SET "libelleZone"=${libelleZone} WHERE "idZone"=${idZone};`
+        clientUsed.query(queryText,[])
+    },
+    //Delete a zone from a festival
+    deleteZone: async (idZone,client)=>{
+        const clientUsed = await DB.getPoolClient(client)
+        const queryText = `DELETE FROM "Zone" WHERE "idZone"=${idZone};`
+        clientUsed.query(queryText,[])
+    },
+    //Get all zones form a festival
+    getAFestivalZones: async (FK_idFestival,client)=>{
+        const clientUsed = await DB.getPoolClient(client)
+        const queryText = `SELECT * FROM "Zone" WHERE "FK_idFestival"=${FK_idFestival};`
+        return (await clientUsed.query(queryText,[]).rows)
+    },
 }
