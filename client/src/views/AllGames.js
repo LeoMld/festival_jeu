6caf1d98f4bba3b4cdbd8c4601ee3bb2bc58b033
leo <lele.mollard@gmail.com>
2021-03-16 11:36:22 +0100

    Front : Login page finalisation manque l'appel au back (pas encore défini les contrôleurs

---
+++ client/src/views/AllGames.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Container} from "reactstrap";
+
+
+function Home(){
+    return(
+        <div className="container justify-content-center">
+            <h1>Tous les jeux</h1>
+
+        </div>
+
+
+
+    )
+}
+
+export default Home;
--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-import {Container} from "reactstrap";
+import {Container, Button} from "reactstrap";
 import logo from "../assets/images/logo_FDJ_FINAL_800.png"
 
 function Login(){
@@ -14,9 +14,10 @@ function Login(){
 
     const REGEXEMAIL = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
-    /*const handleChange = set => event => {
+    const handleChange = set => event => {
         set(event.target.value)
-    }*/
+    }
+
 
     const handleCheckMail = event => {
         setEmail(event.target.value)
@@ -32,24 +33,31 @@ function Login(){
         }
     }
 
+    const onSubmit = () =>{
+        //add route avec email et password
+    }
+
 
 
     return(
         <Container className = "mt-md" >
             <div className="justify-content-center mb-lg">
-                <h1> Se connecter</h1>
+                <h1> Connexion</h1>
                 <div className="row">
-                    <div className="col">
-                        <img style={{maxWidth : "70%"}} src={logo} alt="logo festival du jeu"/>
+                    <div className="col-sm mr-md">
+                        <img style={{maxWidth : "450px"}} src={logo} alt="logo festival du jeu"/>
                     </div>
-                    <div className="col mt-md">
-
+                    <div className="col-sm mt-md">
+                    <form onSubmit={onSubmit}>
                         <div className={"form-group "+ stateIconEmail}>
                             <input onChange={handleCheckMail} value={email} type="text" placeholder="Email" className={"form-control "+ stateEmail}/>
                         </div>
                         <div className="form-group ">
-                            <input value={password} type="password" className="form-control " id="exampleFormControlInput1" placeholder="Mot de passe"/>
+                            <input onChange={handleChange(setPassword)} value={password} type="password" className="form-control " id="exampleFormControlInput1" placeholder="Mot de passe"/>
                         </div>
+                        <button type="button" className="btn btn-success mt-sm-4">Se connecter</button>
+                    </form>
+
                     </div>
                 </div>
 
