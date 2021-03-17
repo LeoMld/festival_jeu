93af4fde21b66ff9763d766ca5f5f4651a76f784
bryan <bryan.barbou@hotmail.fr>
2021-03-17 12:59:43 +0100

    Update GestionRouter

--- server/routes/gestionRoutes.js
+++ server/routes/gestionRoutes.js
@@ -3,7 +3,21 @@ const router = express.Router();
 
 // We import the controller of the gestion routes
 const gestionController = require('../controllers/gestionController');
+const contactController = require("../controllers/contactController");
+const zoneController = require("../controllers/zoneController");
+const personController = require("../controllers/personController");
 
 router.get('/AllFestivals', gestionController.getAllFestivals)
+
+router.get('/editeurs/:id', personController.getEditorPage)
+
+
+router.post('/contact', contactController.createContact)
+router.put('/contact/:id', contactController.updateContact)
+router.delete('/contact/:id', contactController.deleteContact)
+
+router.post('/zone/', zoneController.createNewZone)
+router.put('/zone/:id', zoneController.updateZone)
+router.delete('/zone/:id', zoneController.deleteZone)
 
 module.exports = router;
--- server/routes/homeRoutes.js
+++ server/routes/homeRoutes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 // We import the controller of the homeRoutes routes
 const homeController = require('../controllers/homeController');
 
-router.get('/api', homeController.home)
+
+router.get('/', homeController.home)
+
+
 
 module.exports = router;
