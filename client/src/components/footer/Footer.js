2d6b9485986c7df96cac55fbf892586db2b7d90d
leo <lele.mollard@gmail.com>
2021-03-23 19:25:33 +0100

    front facture réservation

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,21 +1,3 @@
-/*!
-
-=========================================================
-* Argon Design System React - v1.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-design-system-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-/*eslint-disable*/
 import React from "react";
 // reactstrap components
 import {
@@ -33,112 +15,112 @@ class Footer extends React.Component {
   render() {
     return (
 
-      <>
-        <footer className=" footer has-cards">
-          <Container>
-            <Row className=" row-grid align-items-center mb-5">
-              <Col lg="6">
-                <h3 className=" text-primary font-weight-light mb-2">
-                  Thank you for supporting LES FESTIVAUX!
-                </h3>
-                <h4 className=" mb-0 font-weight-light">
-                  Let's get in touch on any of these platforms.
-                </h4>
-              </Col>
-              <Col className="text-lg-center btn-wrapper" lg="6">
-                <Button
-                  className="btn-icon-only rounded-circle"
-                  color="twitter"
-                  href=""
-                  id="tooltip475038074"
-                  target="_blank"
-                >
+        <>
+          <footer className="user-select-none footer has-cards">
+            <Container>
+              <Row className=" row-grid align-items-center mb-5">
+                <Col lg="6">
+                  <h3 className=" text-primary font-weight-light mb-2">
+                    Au plaisir de vous revoir avant l'année 2025 !
+                  </h3>
+                  <h4 className=" mb-0 font-weight-light">
+                    Suivez nous sur nos différents réseaux.
+                  </h4>
+                </Col>
+                <Col className="text-lg-center btn-wrapper" lg="6">
+                  <Button
+                      className="btn-icon-only rounded-circle"
+                      color="twitter"
+                      href=""
+                      id="tooltip475038074"
+                      target="_blank"
+                  >
                   <span className="btn-inner--icon">
                     <i className="fa fa-twitter" />
                   </span>
-                </Button>
-                <UncontrolledTooltip delay={0} target="tooltip475038074">
-                  Follow us
-                </UncontrolledTooltip>
-                <Button
-                  className="btn-icon-only rounded-circle ml-1"
-                  color="facebook"
-                  href=""
-                  id="tooltip837440414"
-                  target="_blank"
-                >
+                  </Button>
+                  <UncontrolledTooltip delay={0} target="tooltip475038074">
+                    Follow us
+                  </UncontrolledTooltip>
+                  <Button
+                      className="btn-icon-only rounded-circle ml-1"
+                      color="facebook"
+                      href=""
+                      id="tooltip837440414"
+                      target="_blank"
+                  >
                   <span className="btn-inner--icon">
                     <i className="fa fa-facebook-square" />
                   </span>
-                </Button>
-                <UncontrolledTooltip delay={0} target="tooltip837440414">
-                  Like us
-                </UncontrolledTooltip>
-                <Button
-                  className="btn-icon-only rounded-circle ml-1"
-                  color="dribbble"
-                  href=""
-                  id="tooltip829810202"
-                  target="_blank"
-                >
+                  </Button>
+                  <UncontrolledTooltip delay={0} target="tooltip837440414">
+                    Like us
+                  </UncontrolledTooltip>
+                  <Button
+                      className="btn-icon-only rounded-circle ml-1"
+                      color="dribbble"
+                      href=""
+                      id="tooltip829810202"
+                      target="_blank"
+                  >
                   <span className="btn-inner--icon">
                     <i className="fa fa-dribbble" />
                   </span>
-                </Button>
-                <UncontrolledTooltip delay={0} target="tooltip829810202">
-                  Follow us
-                </UncontrolledTooltip>
-                <Button
-                  className="btn-icon-only rounded-circle ml-1"
-                  color="github"
-                  href="https://github.com/LeoMld/festival_jeu"
-                  id="tooltip495507257"
-                  target="_blank"
-                >
+                  </Button>
+                  <UncontrolledTooltip delay={0} target="tooltip829810202">
+                    Follow us
+                  </UncontrolledTooltip>
+                  <Button
+                      className="btn-icon-only rounded-circle ml-1"
+                      color="github"
+                      href="https://github.com/LeoMld/festival_jeu"
+                      id="tooltip495507257"
+                      target="_blank"
+                  >
                   <span className="btn-inner--icon">
                     <i className="fa fa-github" />
                   </span>
-                </Button>
-                <UncontrolledTooltip delay={0} target="tooltip495507257">
-                  Star on Github
-                </UncontrolledTooltip>
-              </Col>
-            </Row>
-            <hr />
-            <Row className=" align-items-center justify-content-md-between">
-              <Col md="6">
-                <div className=" copyright">
-                  © {new Date().getFullYear()}{" "}
-                  <a
-                    href="https://www.polytech.umontpellier.fr/"
-                    target="_blank"
-                  >
-                    Polytech
-                  </a>
-                  .
-                </div>
-              </Col>
-              <Col md="6">
-                <Nav className=" nav-footer justify-content-end">
-                  <NavItem>
-
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      href=""
-                      target="_blank"
+                  </Button>
+                  <UncontrolledTooltip delay={0} target="tooltip495507257">
+                    Star on Github
+                  </UncontrolledTooltip>
+                </Col>
+              </Row>
+              <hr />
+              <Row className=" align-items-center justify-content-md-between">
+                <Col md="6">
+                  <div className=" copyright">
+                    © {new Date().getFullYear()}{" "}
+                    <a
+                        href="https://www.polytech.umontpellier.fr/"
+                        target="_blank"
                     >
-                      About Us
-                    </NavLink>
-                  </NavItem>
+                      Polytech
+                    </a>
+                    .
+                  </div>
+                </Col>
+                <Col md="6">
+                  <Nav className=" nav-footer justify-content-end">
+                    <NavItem>
+
+                    </NavItem>
+                    <NavItem>
+                      <NavLink
+                          href=""
+                          target="_blank"
+                      >
+                        About Us
+                      </NavLink>
+                    </NavItem>
 
 
-                </Nav>
-              </Col>
-            </Row>
-          </Container>
-        </footer>
-      </>
+                  </Nav>
+                </Col>
+              </Row>
+            </Container>
+          </footer>
+        </>
     );
   }
 }
