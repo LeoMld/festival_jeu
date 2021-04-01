1da677a31ccf46dca1b2dac5c9c2eb08b2ef2bea
leo <lele.mollard@gmail.com>
2021-04-01 21:42:26 +0200

    fix :Filtre liste des jeux visiteurs affiche toute la liste des jeux et pas seulement la liste des jeux du festival courant OK fix : Creation de jeux (id auto-increment était non fonctionnel) OK fix Modif profil : Si input vide, errreur serveur  OK

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -80,25 +80,31 @@ module.exports = {
         const prenom = req.sanitize(req.body.prenom)
         const email = req.sanitize(req.body.mail)
         let pwd = req.sanitize(req.body.pwd)
+        const REGEXEMAIL = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
-        const user = await usersModel.searchUser(email)
+        if(email && email.match(REGEXEMAIL)){
+            const user = await usersModel.searchPwdUser(idUtilisateur)
 
-        if(!pwd){
-            pwd= " "
-        }
+            if(!pwd){
+                pwd= " "
+            }
 
-        const match = await bcrypt.compare(pwd, user[0].mdpUtilisateur);
-        if(match){
-            usersModel.changeInfosUser(idUtilisateur, nom,prenom,email)
-                .then(() => {
-                    res.status(200).json()
-                })
-                .catch((err) => {
-                    res.status(503).json()
-                })
+            const match = await bcrypt.compare(pwd, user.mdpUtilisateur);
+            if(match){
+                usersModel.changeInfosUser(idUtilisateur, nom,prenom,email)
+                    .then(() => {
+                        res.status(200).json()
+                    })
+                    .catch((err) => {
+                        res.status(503).json()
+                    })
+            }else{
+                res.status(401).json()
+            }
         }else{
-            res.status(401).json()
+            res.status(400).json()
         }
+
 
     },
     // We update the mail and password
